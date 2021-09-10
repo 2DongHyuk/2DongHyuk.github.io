819a7325cab0ff037842a291f710a6770ec7f4e9
2DongHyuk <87635770+2DongHyuk@users.noreply.github.com>
2021-09-10 15:14:42 +0900

    g
    ggg

--- src/Card.js
+++ src/Card.js
@@ -19,8 +19,8 @@ export default function Card() {
           />
         </div>
         <div className="text-center mt-5">
-          <Name name="Jaemyun Lee" />
-          <Position title="Software Engineer" />
+          <Name name="starbucks" />
+          <Position title="coffee" />
           <div className="flex justify-center mt-4">
             <div className="text-xl m-1 p-1 sm:m-2 sm:p-2 text-gray-800 hover:bg-gray-800 rounded-full hover:text-white ">
               <FaCode />
